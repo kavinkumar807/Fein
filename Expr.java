954676e002fb2043c5628620a0a5d606aceb08e9
kavinkumar807 <61575461+kavinkumar807@users.noreply.github.com>
2024-08-15 23:20:24 +0530

    implemented-ast-printer
    * implemented ast printer
    * visitor pattern
    * implemented visitor pattern in generate ast

--- Expr.java
+++ Expr.java
@@ -1,6 +1,12 @@
 import java.util.List;
 
 abstract class Expr {
+  interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+    R visitGroupingExpr(Grouping expr);
+    R visitLiteralExpr(Literal expr);
+    R visitUnaryExpr(Unary expr);
+  }
  static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
         this.left = left;
@@ -8,13 +14,23 @@ abstract class Expr {
         this.right = right;
     }
 
-        final Expr left;
-        final Token operator;
-        final Expr right;
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this);
+    }
+
+    final Expr left;
+    final Token operator;
+    final Expr right;
     }
  static class Grouping extends Expr {
     Grouping(Expr expression) {
         this.expression = expression;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this);
     }
 
     final Expr expression;
@@ -22,6 +38,11 @@ abstract class Expr {
  static class Literal extends Expr {
     Literal(Object value) {
         this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLiteralExpr(this);
     }
 
     final Object value;
@@ -32,7 +53,14 @@ abstract class Expr {
         this.right = right;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitUnaryExpr(this);
+    }
+
     final Token operator;
     final Expr right;
     }
+
+  abstract <R> R accept(Visitor<R> visitor);
 }
